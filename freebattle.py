bb05fcfebda8972eafb0423bddb5b8c860d49982
Stevanus Cahya Anggara <stevanuschyaanggara@gmail.com>
2025-05-06 08:41:22 +0700

    BENERIN INI MAS

--- freebattle.py
+++ freebattle.py
@@ -1,61 +1,155 @@
-
 import pygame
+from pygame import mixer
+from fighter import Fighter
 
+mixer.init()
 pygame.init()
 
+#create game window
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
 
-screen_width = 800
-screen_height = 600
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("FREE BATTLE")
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Brawler")
+
+#set framerate
+clock = pygame.time.Clock()
+FPS = 60
+
+#define colours
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+
+#define game variables
+intro_count = 3
+last_count_update = pygame.time.get_ticks()
+score = [0, 0]#player scores. [P1, P2]
+round_over = False
+ROUND_OVER_COOLDOWN = 2000
+
+#define fighter variables
+WARRIOR_SIZE = [48,48] 
+WARRIOR_SCALE = 3 
+WARRIOR_OFFSET = [10,5]
+WARRIOR_DATA = [WARRIOR_SIZE[0], WARRIOR_SIZE[1],WARRIOR_SCALE, WARRIOR_OFFSET]
+WIZARD_SIZE = [48,48]
+WIZARD_SCALE = 3
+WIZARD_OFFSET = [10,5]
+WIZARD_DATA = [WIZARD_SIZE[0], WIZARD_SIZE[1],WIZARD_SCALE, WIZARD_OFFSET]
+
+#load music and sounds
+pygame.mixer.music.load("assets/audio/music.mp3")
+pygame.mixer.music.set_volume(0.5)
+pygame.mixer.music.play(-1, 0.0, 5000)
+sword_fx = pygame.mixer.Sound("assets/audio/sword.wav")
+sword_fx.set_volume(0.5)
+magic_fx = pygame.mixer.Sound("assets/audio/magic.wav")
+magic_fx.set_volume(0.75)
+
+#load background image
+bg_image = pygame.image.load("assets/images/background/background.jpg").convert_alpha()
+
+#load spritesheets
+warrior_sheet = pygame.image.load("assets/images/warrior/Sprites/warrior4r.png").convert_alpha()
+wizard_sheet = pygame.image.load("assets/images/warrior/Sprites/warrior4r.png").convert_alpha()
+
+#load vicory image
+victory_img = pygame.image.load("assets/images/icons/victory.png").convert_alpha()
+
+#define number of steps in each animation
+WARRIOR_ANIMATION_STEPS = [10,8,1,7,7,3,7]
+WIZARD_ANIMATION_STEPS = [10,8,1,7,7,3,7]
+
+#define font
+count_font = pygame.font.Font("assets/fonts/turok.ttf", 80)
+score_font = pygame.font.Font("assets/fonts/turok.ttf", 30)
+
+#function for drawing text
+def draw_text(text, font, text_col, x, y):
+  img = font.render(text, True, text_col)
+  screen.blit(img, (x, y))
+
+#function for drawing background
+def draw_bg():
+  scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+  screen.blit(scaled_bg, (0, 0))
+
+#function for drawing fighter health bars
+def draw_health_bar(health, x, y):
+  ratio = health / 100
+  pygame.draw.rect(screen, WHITE, (x - 2, y - 2, 404, 34))
+  pygame.draw.rect(screen, RED, (x, y, 400, 30))
+  pygame.draw.rect(screen, YELLOW, (x, y, 400 * ratio, 30))
 
 
-white = (255, 255, 255)
-black = (0, 0, 0)
-button_color = (80, 80, 80)
-text_color = (200, 200, 200)
+#create two instances of fighters
+fighter_1 = Fighter(1, 200, 400, False, WARRIOR_DATA, warrior_sheet, WARRIOR_ANIMATION_STEPS, sword_fx, is_bot=False)
+fighter_2 = Fighter(2, 700, 400, True, WIZARD_DATA, wizard_sheet, WIZARD_ANIMATION_STEPS, magic_fx, is_bot=False)
+
+#game loop
+run = True
+while run:
+
+  clock.tick(FPS)
+
+  #draw background
+  draw_bg()
+
+  #show player stats
+  draw_health_bar(fighter_1.health, 20, 20)
+  draw_health_bar(fighter_2.health, 580, 20)
+  draw_text("P1: " + str(score[0]), score_font, RED, 20, 60)
+  draw_text("P2: " + str(score[1]), score_font, RED, 580, 60)
+
+  #update countdown
+  if intro_count <= 0:
+    #move fighters
+    fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_2, round_over)
+    fighter_2.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_1, round_over)
+  else:
+    #display count timer
+    draw_text(str(intro_count), count_font, RED, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 3)
+    #update count timer
+    if (pygame.time.get_ticks() - last_count_update) >= 1000:
+      intro_count -= 1
+      last_count_update = pygame.time.get_ticks()
+
+  #update fighters
+  fighter_1.update()
+  fighter_2.update()
+
+  #draw fighters
+  fighter_1.draw(screen)
+  fighter_2.draw(screen)
+
+  #check for player defeat
+  if round_over == False:
+    if fighter_1.alive == False:
+      score[1] += 1
+      round_over = True
+      round_over_time = pygame.time.get_ticks()
+    elif fighter_2.alive == False:
+      score[0] += 1
+      round_over = True
+      round_over_time = pygame.time.get_ticks()
+  else:
+    #display victory image
+    screen.blit(victory_img, (360, 150))
+    if pygame.time.get_ticks() - round_over_time > ROUND_OVER_COOLDOWN:
+      round_over = False
+      intro_count = 3
+      fighter_1 = Fighter(1, 200, 400, False, WARRIOR_DATA, warrior_sheet, WARRIOR_ANIMATION_STEPS, sword_fx,is_bot=False)
+      fighter_2 = Fighter(2, 700, 400, True, WIZARD_DATA, wizard_sheet, WIZARD_ANIMATION_STEPS, magic_fx,is_bot=True)
+
+  #event handler
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      run = False
 
 
-font = pygame.font.Font(None, 36)
+  #update display
+  pygame.display.update()
 
-coming_soon_text = font.render("COMING SOON", True, black)
-text_rect = coming_soon_text.get_rect(center=(screen_width // 2, screen_height // 2 - 50))
-
-
-back_button_width = 200
-back_button_height = 50
-back_button_x = screen_width // 2 - back_button_width // 2
-back_button_y = screen_height - 100
-back_button_rect = pygame.Rect(back_button_x, back_button_y, back_button_width, back_button_height)
-back_button_text = font.render("Kembali ke Menu", True, text_color)
-back_button_text_rect = back_button_text.get_rect(center=back_button_rect.center)
-
-def jalankan_story():
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if back_button_rect.collidepoint(event.pos):
-                    print("Tombol 'Kembali ke Menu' diklik!")
-                    running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    running = False
-
-        screen.fill(white)
-
-        
-        screen.blit(coming_soon_text, text_rect)
-
-        pygame.draw.rect(screen, button_color, back_button_rect)
-        screen.blit(back_button_text, back_button_text_rect)
-
-        
-        pygame.display.flip()
-
-    pygame.quit() 
-
-if __name__ == '__main__':
-    jalankan_story()
+#exit pygame
+pygame.quit()
